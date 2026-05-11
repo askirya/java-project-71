eac01130856e6814c69d727c92861b6f5d4f9888
Ippach <ippahossawi@gmail.com>
2026-05-11 14:40:20 +0300

    Этап 3. Вывод справки.

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,22 @@
 package hexlet.code;
 
-public class App {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+
+@Command(
+    name = "gendiff",
+    mixinStandardHelpOptions = true,
+    version = "gendiff 1.0",
+    description = "Compares two configuration files and shows a difference."
+)
+public class App implements Runnable {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        var commandLine = new CommandLine(new App());
+        commandLine.execute(args);
+    }
+
+    @Override
+    public void run() {
+        // The command currently only supports service options.
     }
 }
